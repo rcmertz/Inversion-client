3e7330006f858e6d42a674c210452238b9300e5b
Layal Youssef Safa Al Wanni <NISSOLELO03@GMAIL.COM>
2023-05-08 21:30:40 -0300

    index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,12 +12,22 @@ const routes: Array<RouteRecordRaw> = [
     name: "home",
     component: () => import("../views/HomeView.vue"),
   },
+
+  {
+    path: "/produto-list",
+    name: "produto-list",
+    component: () => import("../views/carteira/"),
+  },
+  {
+    path: "/produto-cadastro",
+    name: "produto-cadastro",
+    component: () => import("../views/produto/produto-cadastro.vue"),
+  },
 ];
 
-
 const router = createRouter({
-   history: createWebHistory(process.env.BASE_URL),
-   routes,
- });
- 
- export default router;
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
